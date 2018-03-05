fd775173fa885c0fd6d5e83daf0d48633d54a769
raviSussol <ravi.shrestha@sussol.net>
2018-03-05 17:11:05 +0545

    Updated webpack file name

---
+++ webpack.config.js
@@ -0,0 +1,5 @@
+import path from 'path';
+import webpack from 'webpack';
+import { CommonsChunkPlugin } from 'webpack/optimize';
+import { HotModuleReplacementPlugin } from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
