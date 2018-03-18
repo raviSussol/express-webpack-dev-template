b3a03fa370750376a57ef8074933c7144e60f3c2
raviSussol <ravi.shrestha@sussol.net>
2018-03-18 22:13:16 +0545

    Optimized a bit in the previous changes

--- bundler/webpack.config.js
+++ bundler/webpack.config.js
@@ -1,6 +1,5 @@
 /* global __dirname */
 import path from 'path';
-// const ContextReplacementPlugin = require('webpack').ContextReplacementPlugin;
 
 const env = process.env.NODE_ENV || 'development';
 const isProd = env === 'production';
@@ -54,10 +53,5 @@ module.exports = {
     warnings: false,
   },
   mode: env,
-  // devServer: {
-  //   port: process.env.WEBPACK_SERVER_PORT || 8080,
-  //   contentBase: path.join(__dirname, 'build'),
-  //   publicPath: '', // match the output `publicPath`
-  // },
   devtool: isProd ? 'cheap-module-source-map' : 'eval',
 };
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import path from 'path';
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddlware from 'webpack-hot-middleware';
-import config from '../bundler/transpiler';
+import config from '../bundler';
 
 const PORT = 3000;
 const app = express();
@@ -13,13 +13,8 @@ const compiler = webpack(config);
 app.use(webpackDevMiddleware(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath,
-  // contentBase: config.output.contentBase,
 }));
 app.use(webpackHotMiddlware(compiler));
-// app.get('/', (req, res) => res.send('Hi there!'));
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../src/index.html'));
-// });
 app.use(express.static(path.join(__dirname, '../src/index.html')));
 
 app.listen(PORT, () => {
